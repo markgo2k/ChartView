1d58e4f08caf363cfc8e7611589cfb6a6ad62971
Mark Gollin <markgo@gecko.net>
2022-01-15 20:14:37 -0800

    a ChartBase support for preNormalized data

--- Sources/SwiftUICharts/Base/Extensions/ChartBase+Extension.swift
+++ Sources/SwiftUICharts/Base/Extensions/ChartBase+Extension.swift
@@ -5,15 +5,17 @@ extension View where Self: ChartBase {
 	/// Set data for a chart
 	/// - Parameter data: array of `Double`
 	/// - Returns: modified `View` with data attached
-    public func data(_ data: [Double]) -> some View {
+    public func data(_ data: [Double], preNormalized: Bool = false) -> some View {
         chartData.data = data.map { ("", $0) }
+        chartData.isNormalized = preNormalized
         return self
             .environmentObject(chartData)
             .environmentObject(ChartValue())
     }
 
-    public func data(_ data: [(String, Double)]) -> some View {
+    public func data(_ data: [(String, Double)], preNormalized: Bool = false) -> some View {
         chartData.data = data
+        chartData.isNormalized = preNormalized
         return self
             .environmentObject(chartData)
             .environmentObject(ChartValue())
