7a51d839d366beab0124b31ae42942b3f8a31af6
Mark Gollin <markgo@gecko.net>
2022-01-15 20:08:03 -0800

    a preNormalized flag to ChartData to support fixed range

--- Sources/SwiftUICharts/Base/Chart/ChartData.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartData.swift
@@ -3,6 +3,7 @@ import SwiftUI
 /// An observable wrapper for an array of data for use in any chart
 public class ChartData: ObservableObject {
     @Published public var data: [(String, Double)] = []
+    @Published public var isNormalized: Bool = false
 
     var points: [Double] {
         data.map { $0.1 }
@@ -13,12 +14,18 @@ public class ChartData: ObservableObject {
     }
 
     var normalisedPoints: [Double] {
+        if  self.isNormalized {
+            return points
+        }
         let absolutePoints = points.map { abs($0) }
         return points.map { $0 / (absolutePoints.max() ?? 1.0) }
     }
 
     var normalisedRange: Double {
-        (normalisedPoints.max() ?? 0.0) - (normalisedPoints.min() ?? 0.0)
+        let min = isNormalized ? 0.0 : normalisedPoints.min()
+        let max = isNormalized ? 1.0 : normalisedPoints.max()
+
+        return (max ?? 0.0) - (min ?? 0.0)
     }
 
     var isInNegativeDomain: Bool {
@@ -27,11 +34,13 @@ public class ChartData: ObservableObject {
 
     /// Initialize with data array
     /// - Parameter data: Array of `Double`
-    public init(_ data: [Double]) {
+    public init(_ data: [Double], preNormalized: Bool = false) {
+        self.isNormalized = preNormalized
         self.data = data.map { ("", $0) }
     }
 
-    public init(_ data: [(String, Double)]) {
+    public init(_ data: [(String, Double)], preNormalized: Bool = false) {
+        self.isNormalized = preNormalized
         self.data = data
     }
 
